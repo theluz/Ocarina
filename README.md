47a76661713db6b0daefa4ee042001d9f5190eb5
theluz <ti.rodrigoluz@gmail.com>
2019-07-25 17:03:13 -0300

    Fixed not being able to access the audio files in the project. Now it plays the assigned file via code. Working on the best way to play each on a different buttom

---
+++ README.md
@@ -0,0 +1,2 @@
+README - Ocarina. 
+Simple iOS app that simulates an Ocarina from a certain game. Part of Complete iOS App Development Bootcamp course on Udemy.
--- ocarina/ViewController.swift
+++ ocarina/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+  
+    @IBAction func notePressed(_ sender: UIButton) {
+        var sound = sender.tag
+        playSound()
+    }
 
-
+    func playSound(){
+        
+        let url = Bundle.main.url(forResource: "a", withExtension: "wav")
+        
+        do {
+            try player = AVAudioPlayer(contentsOf: url!)
+        }
+        catch {
+            print(error)
+        }
+        player.play()
+    }
 }
+//        let note = sender.tag
+//        switch note {
+//        case note: 1
+//
+//        case note: 2
+//
+//        case note: 3
+//
+//        case note: 4
+//
+//        case note: 5
+//
+//        default: break
+//        }
+
 
